d508c92e4f2061514de5f3516519085ade5231e7
sakamotosora0116 <92964241+sakamotosora0116@users.noreply.github.com>
2021-12-08 14:44:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 $(function() {
     $("#plan-slick").slick({
         infinite: true,
-        //↓一度にひょうじするスライドの数
         slidesToShow: 3,
-        //↓一度にスクロールするスライドの数
         slidesToScroll: 3,
-        //↓スライダーが自動で回る、そのスピード
         autoplay: true,
         autoplaySpeed: 5000,
         speed: 1000,
-        //矢印を消す
         arrows: false,
-        //↓この二つを指定することでセンターモード時のサイドパディングが設定される
         centerMode: true,
         centerPadding: "100px",
         responsive: [
@@ -23,8 +18,6 @@ $(function() {
                 centerMode: true,
                 centerPadding: 0,
                 infinite: true,
-                //↓これが原因で数字がでる
-                // dots: true,
                 }
             },
         ]
@@ -32,7 +25,6 @@ $(function() {
 });
 //header > nav
 $(function() {
-    //できるだけidで指定してあげたい
     $("#nav-btn").on("click",function() {
         $(this).toggleClass("-active");
         $("#nav").toggleClass("-active");
